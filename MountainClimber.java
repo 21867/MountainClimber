24bf5163314492ad4306281613ff56f96a9057de
21867 <43053741+21867@users.noreply.github.com>
2018-09-23 17:24:00 -0700

    Add files via upload

---
+++ MountainClimber.java
@@ -0,0 +1,58 @@
+/**
+   @Author:
+      Date: Sep 07, 2006
+   Teacher:
+       Lab:
+      Misc:
+  */
+
+import kareltherobot.*;
+
+public class MountainClimber implements Directions
+{
+    public static void task(UrRobot karel) 
+  {
+    karel.move();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.move();
+    karel.putBeeper();
+    karel.move();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.putBeeper();
+    karel.turnLeft();
+    karel.move();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.turnLeft();
+    karel.move();
+    karel.move();
+    karel.turnOff();
+    
+    //add your code here
+        
+       
+       
+  } 
+  
+
+ 
+
+
+}
